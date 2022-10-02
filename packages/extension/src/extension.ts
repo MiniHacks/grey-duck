ea5210525d7bfaf5046bf7ed903049c987c1e11a
minizou <gh.mini.zou@gmail.com>
2022-10-01 21:08:48 -0400

    🥗🥗🥗 Remove unnnecessary code actions

--- packages/extension/src/extension.ts
+++ packages/extension/src/extension.ts
@@ -7,12 +7,6 @@ import { subscribeToDocumentChanges, EMOJI_MENTION } from './diagnostics';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	context.subscriptions.push(
-		vscode.languages.registerCodeActionsProvider('python', new Emojizer(), {
-			providedCodeActionKinds: Emojizer.providedCodeActionKinds
-		})
-	);
 
 	const emojiDiagnostics = vscode.languages.createDiagnosticCollection("emoji");
 	context.subscriptions.push(emojiDiagnostics);
@@ -52,59 +46,6 @@ export function activate(context: vscode.ExtensionContext) {
 export function deactivate() {}
 
 const COMMAND = 'code-actions-sample.command';
-
-/**
- * Provides code actions for converting :) to a smiley emoji.
- */
-export class Emojizer implements vscode.CodeActionProvider {
-
-	public static readonly providedCodeActionKinds = [
-		vscode.CodeActionKind.QuickFix
-	];
-
-	public provideCodeActions(document: vscode.TextDocument, range: vscode.Range): vscode.CodeAction[] | undefined {
-		if (!this.isAtStartOfSmiley(document, range)) {
-			return;
-		}
-
-		const replaceWithSmileyCatFix = this.createFix(document, range, '😺');
-
-		const replaceWithSmileyFix = this.createFix(document, range, '😀');
-		// Marking a single fix as `preferred` means that users can apply it with a
-		// single keyboard shortcut using the `Auto Fix` command.
-		replaceWithSmileyFix.isPreferred = true;
-
-		const replaceWithSmileyHankyFix = this.createFix(document, range, '💩');
-
-		const commandAction = this.createCommand();
-
-		return [
-			replaceWithSmileyCatFix,
-			replaceWithSmileyFix,
-			replaceWithSmileyHankyFix,
-			commandAction
-		];
-	}
-
-	private isAtStartOfSmiley(document: vscode.TextDocument, range: vscode.Range) {
-		const start = range.start;
-		const line = document.lineAt(start.line);
-		return line.text[start.character] === ':' && line.text[start.character + 1] === ')';
-	}
-
-	private createFix(document: vscode.TextDocument, range: vscode.Range, emoji: string): vscode.CodeAction {
-		const fix = new vscode.CodeAction(`Convert to ${emoji}`, vscode.CodeActionKind.QuickFix);
-		fix.edit = new vscode.WorkspaceEdit();
-		fix.edit.replace(document.uri, new vscode.Range(range.start, range.start.translate(0, 2)), emoji);
-		return fix;
-	}
-
-	private createCommand(): vscode.CodeAction {
-		const action = new vscode.CodeAction('Learn more...', vscode.CodeActionKind.Empty);
-		action.command = { command: COMMAND, title: 'Learn more about emojis', tooltip: 'This will open the unicode emoji page.' };
-		return action;
-	}
-}
 
 /**
  * Provides code actions corresponding to diagnostic problems.
