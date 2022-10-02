10642da333e061b7e4a6a7d647ad52651ed138e4
Samyok Nepal <sam@yok.dev>
2022-10-02 09:47:16 -0400

    🤪 make frontend work

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,21 +1,29 @@
 /* eslint-disable prettier/prettier */
-import type { NextPage } from "next";
-import { Text, Flex, Heading, Button, Image } from "@chakra-ui/react";
-import { Cards } from '../components/Cards';
+import type {NextPage} from "next";
+import {Text, Flex, Heading, Button, Image, VStack} from "@chakra-ui/react";
+import {Cards} from '../components/Cards';
 import PageLayout from "../components/Layout/PageLayout";
+import {useCallback} from "react";
 
 const Home: NextPage = () => {
+  const onClick = useCallback(() => {
+    // eslint-disable-next-line no-alert
+    if (window.innerWidth < 768) return alert("Please use a desktop browser to use this feature");
+    return fetch("https://greygoose.guide/create-session").then(r => r.json()).then((res) => {
+      window.open(res.url, "_blank");
+    });
+  }, []);
   return (
     <PageLayout title={"geese, by minihacks"}>
-      <Flex 
+      <Flex
         bgGradient={"linear(to-br, #B1A17B, #7C62EE, #C25BD4)"}
         direction={"column"}
         align={"center"}
         px={[5, 10]} py={24}
         h={"360vh"}>
-        
+
         <Flex direction={"column"} align={"end"}>
-          <Text 
+          <Text
             color={"white"}
             fontSize={"9xl"}
             fontWeight={"bold"}>
@@ -26,19 +34,20 @@ const Home: NextPage = () => {
           </Heading>
         </Flex>
 
-        <Button p={4} size={"lg"} bg={"#010318"} color={"white"}>
-            give it a try
+        <Button px={12} size={"lg"} bg={"#010318"} color={"white"} onClick={onClick}>
+          give it a try
         </Button>
 
-        <Flex justify={"center"} mx={32} my={44} px={[5, 10]}>
-          <Heading textAlign={"center"} color={"white"} mb={2} size={"2xl"}>
-            Grey Duck is a VSCode extension that will provide feedback on code
-            that you write, allowing you to learn how to write better instead of
-            more.
+        <VStack justify={"center"} mx={32} my={44} px={[5, 10]}>
+          <Heading textAlign={"center"} color={"white"} mb={1} size={"2xl"}>
+            We provide feedback on code that you write
           </Heading>
-        </Flex>
-       
-        <Cards />
+          <Heading textAlign={"center"} color={"white"} size={"lg"}>allowing you to learn how to
+            write <i>better</i> instead of
+            {" "}<i>more</i>.</Heading>
+        </VStack>
+
+        <Cards/>
 
         <Image src="pixel_goose_animated.gif" alt="pixel goose"/>
 
