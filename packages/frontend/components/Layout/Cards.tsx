4fe82cec873e4f2248b4de0f68ad78340dae24e2
minizou <gh.mini.zou@gmail.com>
2022-10-02 06:05:43 -0400

    Add sembelance of a landing 🍧🍧

---
+++ packages/frontend/components/Layout/Cards.tsx
@@ -0,0 +1,38 @@
+import { Box, SimpleGrid, Text, Image } from "@chakra-ui/react";
+
+interface Props {
+  title: string;
+  caption: string;
+}
+
+const Card = ({ title, caption: text }: Props) => {
+  return (
+    <Box boxShadow={"lg"} p={8} bg={"white"} borderRadius={"16px"}>
+      <Image  
+        boxSize={"200px"}
+        src={""} alt={""} />
+      <Text fontWeight={"bold"}>{title}</Text>
+      <Text>{text}</Text>
+    </Box>
+  );
+};
+
+export const Cards = () => {
+  return (
+    <Box px={4} py={12}>
+      <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
+        <Card
+          title={"1. Write some code"}
+          caption={`Write three for-loops in a row and contemplate whether you're 
+            actually a competent programmer.`}/>
+        <Card
+          title={"2. Receive suggestions"}
+          caption={`Let the grey duck honk suggestions at you and realize that maybe
+            40 if statements was not the most optimal solution after all.`}/>
+        <Card
+          title={"3. Become numpy expert"}
+          caption={"happy x600 speed improvement"}/>
+      </SimpleGrid>
+    </Box>
+  );
+};
--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,13 +1,46 @@
+/* eslint-disable prettier/prettier */
 import type { NextPage } from "next";
-import { Box, Heading } from "@chakra-ui/react";
+import { Text, Box, Flex, Heading, Button } from "@chakra-ui/react";
+import { Cards } from '../components/Layout/Cards';
 import PageLayout from "../components/Layout/PageLayout";
 
 const Home: NextPage = () => {
   return (
     <PageLayout title={"geese, by minihacks"}>
-      <Box px={[5, 10]}>
-        <Heading as={"h1"}>geese</Heading>
-      </Box>
+      <Flex 
+        bgGradient={"linear(to-br, #B1A17B, #7C62EE, #C25BD4)"}
+        direction={"column"}
+        align={"center"}
+        px={[5, 10]} py={24}
+        h={"240vh"}>
+        
+        <Flex direction={"column"} align={"end"}>
+          <Text 
+            color={"white"}
+            fontSize={"9xl"}
+            fontWeight={"bold"}>
+            grey duck.
+          </Text>
+          <Heading mb={20} size={"lg"}>
+            your mentor, line-by-line
+          </Heading>
+        </Flex>
+
+        <Button size={"lg"} bg={"#010318"} color={"white"}>
+            give it a try
+        </Button>
+
+        <Flex justify={"center"} mx={32} my={44} px={[5, 10]}>
+          <Heading textAlign="center" color={"white"} mb={2} size={"2xl"}>
+            Grey Duck is a VSCode extension that will provide feedback on code
+            that you write, allowing you to learn how to write better instead of
+            more.
+          </Heading>
+        </Flex>
+       
+        <Cards />
+      
+      </Flex>
     </PageLayout>
   );
 };
